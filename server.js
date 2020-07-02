919cca5951d19c094c75279f3da9c27111c50959
suneethaburla <suneetha@gmail.com>
2020-07-02 16:11:17 -0700

    added models, seeders, controllers

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require ("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.port||3000;
-
 const app = express();
 
-app.use (logger("dev"));
+// If we are in production (Heroku), process.env.PORT is true, 
+// If we are in development it is false, default to 3000
+const PORT = process.env.port||3000;
 
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+app.use (logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes)
+
 mongoose.connect (process.env.MONGODB_URI || 'mongodb://localhost/fitness_db',
-{ useNewUrlParser: true },
-{ useUnifiedTopology: true }
+{ useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-app.listen(PORT, ()=> console.log(`server started on ${PORT}`));
+app.listen(PORT, ()=> console.log(`server started on Port: ${PORT}`));
