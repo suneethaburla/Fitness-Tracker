43b2c9e2a4c872cb72748a2502fefcfce2aa4fde
suneethaburla <suneetha@gmail.com>
2020-07-02 17:03:14 -0700

    added routes for getworkoutsinrange and addexercise

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 
-const { getWorkout, createWorkout} = require("./../controllers/workoutController")
+const { getWorkout, createWorkout, getWorkoutsInRange, addExercise} = require("./../controllers/workoutController")
 
 
 router.route("/workouts")
         .get(getWorkout)
         .post(createWorkout)
+
+router.route("/workouts/range")
+        .get(getWorkoutsInRange)
+
+router.route("/workouts/:id")
+        .put(addExercise)
 
 module.exports = router;
