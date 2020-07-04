131b39edd1ef19b605bc7e7541100962ba6c4eb8
suneethaburla <suneetha@gmail.com>
2020-07-04 13:16:05 -0700

    updated server.js

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes)
 
+// Connect database
 mongoose.connect (process.env.MONGODB_URI || 'mongodb://localhost/fitness_db',
 { useNewUrlParser: true, useUnifiedTopology: true }
 );
